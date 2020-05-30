295de2f319639fae8afe1b67ad8c55d11d2f12c8
jtaenzer <mars.taenzer@gmail.com>
2020-05-30 19:05:23 -0400

    Working version for only two known job titles

--- JobTitleClustering/JobTitleClustering/linkage_example.py
+++ JobTitleClustering/JobTitleClustering/linkage_example.py
@@ -8,39 +8,31 @@ writeDFs = True
 
 data_extractor = Pipeline("FutureFitAI_database", "talent_profiles")
 print("Getting skills")
-padded_data, data = data_extractor.get_skills_list_of_lists_by_titles(["data scientist", "data engineer"])
-# padded_data, data = data_extractor.get_all_skills()
+titles, data, data_flat = data_extractor.get_skills_by_titles(["data scientist", "data engineer"])
+# titles, data, data_flat = data_extractor.get_all_skills()
 
 print("Preprocessing data")
-titles = padded_data[:, 0]
-skills = padded_data[:, 1:]
-skills_flat = skills[skills != None].tolist()
-labelencoder = LabelEncoder()
-labelencoder.fit(skills_flat)
-# encode the non-padded data since our encoding doesn't know about "None" which we used to pad
-# transforming row[1:] here because row[0] contains the job title which our encoding doesn't know about
-skills_encoded = [labelencoder.transform(row[1:]) for row in data]
-data_df_encoded = pd.DataFrame(0, index=np.arange(len(skills_encoded)), columns=labelencoder.classes_)
-for index, entry in enumerate(skills_encoded):
-    data_df_encoded.iloc[index, entry] = 1
-data_df_encoded.insert(0, "job title", titles)
+labelenc = LabelEncoder()
+labelenc.fit(pd.Series(data_flat, dtype=str).drop_duplicates().tolist())
+data_encoded = [labelenc.transform(row) for row in data]
+data_encoded_df = pd.DataFrame(0, index=np.arange(len(data_encoded)), columns=labelenc.classes_, dtype=int)
+for index, entry in enumerate(data_encoded):
+    data_encoded_df.iloc[index, entry] = 1
+data_encoded_df.insert(0, "job title", titles)
 
-# Create a reduced dataframe by including only skills that occur in the dataset with some minimum frequency
-skills_series = pd.Series(skills_flat)
-mask = skills_series.isin(skills_series.value_counts()[skills_series.value_counts() > 10].index)
-skills_to_include = skills_series[mask].drop_duplicates()
-data_df_reduced = data_df_encoded[["job title"] + skills_to_include.tolist()]
+# Remove skills that appear with low frequency
+data_flat_series = pd.Series(data_flat, dtype=str)
+mask = data_flat_series.isin(data_flat_series.value_counts()[data_flat_series.value_counts() > 10].index)
+cols_to_include = data_flat_series[mask].drop_duplicates()
+data_reduced_df = data_encoded_df[["job title"] + cols_to_include.tolist()]
 
 if writeDFs:
-    data_df_encoded.to_csv(r'./data_df_encoded.txt', sep=" ", index=False)
-    data_df_reduced.to_csv(r'./data_df_reduced.txt', sep=" ", index=False)
-
-print(data_df_encoded)
-print(data_df_reduced)
+    data_encoded_df.to_csv(r'./data_df_encoded.txt', sep=" ", index=False)
+    data_reduced_df.to_csv(r'./data_df_reduced.txt', sep=" ", index=False)
 
 print("Clustering")
 # Cluster!
-clustering = linkage(categorized_data_df.to_numpy(), method="single", metric="hamming")
+clustering = linkage(data_reduced_df[cols_to_include.tolist()], method="single", metric="hamming")
 with open("matrix.txt", "w+") as file:
     np.savetxt(file, clustering, fmt="%.5f")
 
