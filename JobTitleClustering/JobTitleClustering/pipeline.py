af8086da9dd920ecc7e63389109047995863ff9a
jtaenzer <mars.taenzer@gmail.com>
2020-05-30 19:04:51 -0400

    Major changes to improve memory requirements

--- JobTitleClustering/JobTitleClustering/pipeline.py
+++ JobTitleClustering/JobTitleClustering/pipeline.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient
 import bson
-import numpy as np
 
 """
 pipeline.py extracts information from the mongoDB
@@ -16,9 +15,30 @@ class Pipeline:
 
     # Creates a list of lists where each sub-list contains the skills from a profile in the DB
     # Used to create categorical dataset for the clustering model
-    def get_skills_list_of_lists_by_titles(self, titles=[]):
+    def get_all_skills(self):
+        conditions = dict()
+        conditions["$or"] = [{"skills": {"$exists": True, "$ne": []}},
+                             {"experience.title": {"$exists": True, "$ne": []}}]
+        mask = {"_id": 0, "experience.title.name": 1, "skills": 1}
+        skills_flat = []
+        skills_data = []
+        titles = []
+        for profile in self.collection.find(conditions, mask):
+            try:
+                tmp = [skill["name"] for skill in profile["skills"]]
+                skills_flat.extend(tmp)
+                skills_data.append(tmp)
+                titles.append(profile["experience"][0]["title"]["name"])
+            except (KeyError, IndexError) as err:
+                print("Encountered error while gathering data:", err)
+                continue
+        return titles, skills_data, skills_flat
+
+    # Similar to get_all_skills but allows filtering by known job titles
+    # Current job title filtering is simple: Find job titles starting with strings in the titles list using regex
+    def get_skills_by_titles(self, titles):
         if not titles:
-            return [[]]
+            return
         # Build a list of regex expressions to use in the conditions dictionary for the DB query
         regx_exprs = [bson.regex.Regex("^{}".format(title)) for title in titles]
         conditions = {"skills": {"$exists": True, "$ne": []}}
@@ -28,41 +48,17 @@ class Pipeline:
         conditions["$or"] = or_list
         mask = {"_id": 0, "experience.title.name": 1, "skills": 1}
 
-        # Collection relevant data from the query
-        # Harded-coded [0] is dangerous but the DB should be sanitized to ensure this always exists
-        skills = []
+        # Collect relevant data from the query
+        skills_data = []
+        skills_flat = []
+        titles = []
         for profile in self.collection.find(conditions, mask):
             try:
-                skills.append([profile["experience"][0]["title"]["name"]] + [skill["name"] for skill in profile["skills"]])
-            except (KeyError, IndexError):
+                tmp = [skill["name"] for skill in profile["skills"]]
+                skills_flat.extend(tmp)
+                skills_data.append(tmp)
+                titles.append(profile["experience"][0]["title"]["name"])
+            except (KeyError, IndexError) as err:
+                print("Encountered error while gathering data:", err)
                 continue
-
-        # Create a padded numpy array so we can take advantage of some numpy functionalities
-        skills_arr = np.array(skills, dtype=object)
-        lens = np.array([len(row) for row in skills])
-        array_mask = np.arange(lens.max()) < lens[:, None]
-        skills_arr_padded = np.empty(array_mask.shape, dtype=skills_arr.dtype)
-        skills_arr_padded[:] = None
-        skills_arr_padded[array_mask] = np.concatenate(skills_arr)
-
-        return skills_arr_padded, skills
-
-    def get_all_skills(self):
-        conditions = {"skills": {"$exists": True, "$ne": []}, "experience.title": {"$exists": True, "$ne": []}}
-        mask = {"_id": 0, "experience.title.name": 1, "skills": 1}
-        skills = []
-        for profile in self.collection.find(conditions, mask):
-            try:
-                skills.append([profile["experience"][0]["title"]["name"]] + [skill["name"] for skill in profile["skills"]])
-            except (KeyError, IndexError):
-                continue
-
-        # Create a padded numpy array so we can take advantage of some numpy functionalities
-        skills_arr = np.array(skills, dtype=object)
-        lens = np.array([len(row) for row in skills])
-        array_mask = np.arange(lens.max()) < lens[:, None]
-        skills_arr_padded = np.empty(array_mask.shape, dtype=skills_arr.dtype)
-        skills_arr_padded[:] = np.nan
-        skills_arr_padded[array_mask] = np.concatenate(skills_arr)
-
-        return skills_arr_padded, skills
+        return titles, skills_data, skills_flat
