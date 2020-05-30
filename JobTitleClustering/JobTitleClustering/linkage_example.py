593327b4c19714ccb9d49c46bf9b3944a6e2b743
jtaenzer <mars.taenzer@gmail.com>
2020-05-29 20:31:25 -0400

    Added some comments

--- JobTitleClustering/JobTitleClustering/linkage_example.py
+++ JobTitleClustering/JobTitleClustering/linkage_example.py
@@ -7,19 +7,18 @@ from scipy.cluster.hierarchy import dendrogram, linkage
 data_extractor = Pipeline("FutureFitAI_database", "talent_profiles")
 lol = data_extractor.get_skills_list_of_lists_by_titles(["data scientist", "data engineer"])
 
-all_skills = []
-for entry in lol:
-    all_skills.extend(entry)
-
+# Flatten list of lists and encode skills as integers
+all_skills = [skill for skill_list in lol for skill in skill_list]
 labelencoder = LabelEncoder()
 all_skills_int_encode = labelencoder.fit_transform(all_skills)
 all_skills_int_encode = all_skills_int_encode.reshape(len(all_skills_int_encode), 1)
-
 lol_int_labels = [list(labelencoder.transform(entry)) for entry in lol]
 
-labeled_df = pd.DataFrame(0, index=np.arange(len(lol)), columns=labelencoder.classes_)
-
+# Convert integer labeled data to a dataframe with a column for each skill
+# Rows will contain 0s for skills not in the profile, 1 for skills in the profile
+labeled_df = pd.DataFrame(0, index=np.arange(len(lol_int_labels)), columns=labelencoder.classes_)
 for index, entry in enumerate(lol_int_labels):
     labeled_df.iloc[index, entry] = 1
 
+# Cluster!
 Z = linkage(labeled_df.to_numpy(), method="single", metric="hamming")
