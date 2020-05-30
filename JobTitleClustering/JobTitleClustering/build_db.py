8d47619f40b1e8c2b2433bdb8187b9eff0eb34ed
jtaenzer <mars.taenzer@gmail.com>
2020-05-30 11:56:28 -0400

    Added a query to remove profiles where skills and experience are both empty

--- JobTitleClustering/JobTitleClustering/build_db.py
+++ JobTitleClustering/JobTitleClustering/build_db.py
@@ -9,11 +9,9 @@ build_db.py loops over files in data/raw and adds their contents to a mongoDB
 Consider moving some of the hard coded strings to a config file
 """
 
-# Connect to the MongoDB client
+# Connect to the MongoDB client, create database and a table to hold talent profiles
 myclient = MongoClient("mongodb://localhost:27017/")
-# Get the FutureFitAI_database from MongoClient
 db = myclient["FutureFitAI_database"]
-# Create a talent_profiles table
 collection_profiles = db["talent_profiles"]
 # Create the "id" index as unique to avoid writing duplicate entries to the table
 collection_profiles.create_index("id", unique=True)
@@ -21,13 +19,18 @@ collection_profiles.create_index("id", unique=True)
 data_path = "C:/Users/joe/PycharmProjects/InsightProject/data/raw/"
 data_files = os.listdir(data_path)
 
+# Loop over files, load each line as a json, and insert it to the db
 for data_file in data_files:
     print("Adding file {} to the DB.".format(data_file))
     with open(data_path + data_file) as data_file_content:
         for line in data_file_content:
-            # Try to insert each line (converted to json) to the DB
             try:
                 collection_profiles.insert_one(json.loads(line))
             # continue if the "id" of the line exists already in the table, causing a DuplicateKeyError
             except pymongoerrors.DuplicateKeyError as err:
                 continue
+
+# Remove rows where both the skills and experience arrays are empty
+# Could probably be avoided by inspecting the json files above before inserting
+conditions = {"$and": [{"skills": {"$exists": True, "$eq": []}}, {"experience": {"$exists": True, "$eq": []}}]}
+collection_profiles.remove(conditions)
