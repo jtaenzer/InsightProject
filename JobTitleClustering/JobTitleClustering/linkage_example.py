4e8db141a6f25ad4262f382f2166573f001bd89d
jtaenzer <mars.taenzer@gmail.com>
2020-05-31 12:00:04 -0400

    Restructured to drop low frequency skills before encoding. Added some progress printouts to the long for loops.

--- JobTitleClustering/JobTitleClustering/linkage_example.py
+++ JobTitleClustering/JobTitleClustering/linkage_example.py
@@ -1,3 +1,4 @@
+import sys
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
@@ -8,31 +9,54 @@ writeDFs = True
 
 data_extractor = Pipeline("FutureFitAI_database", "talent_profiles")
 print("Getting skills")
-titles, data, data_flat = data_extractor.get_skills_by_titles(["data scientist", "data engineer"])
+# titles, data, data_flat = data_extractor.get_skills_by_titles(["data scientist", "data engineer"])
+titles, data, data_flat = data_extractor.get_skills_by_titles(["data scientist", "data engineer", "marketing manager", "brand manager"])
 # titles, data, data_flat = data_extractor.get_all_skills()
 
-print("Preprocessing data")
+print("Preprocessing - creating mask")
+data_flat_ser = pd.Series(data_flat, dtype=str)
+mask = data_flat_ser.isin(data_flat_ser.value_counts()[data_flat_ser.value_counts() > 10].index)
+
+print("Preprocessing - reducing flat data")
+data_flat_reduced = data_flat_ser[mask]
+
+print("Preprocessing - fitting integer encoding")
 labelenc = LabelEncoder()
-labelenc.fit(pd.Series(data_flat, dtype=str).drop_duplicates().tolist())
-data_encoded = [labelenc.transform(row) for row in data]
+labelenc.fit(data_flat_reduced.drop_duplicates().tolist())
+
+print("Preprocessing - encoding data row by row")
+data_encoded = []
+for index, row in enumerate(data):
+    sys.stdout.write("\r")
+    sys.stdout.write("{:2.0f}".format(float(index/len(data))*100) + "%")
+    row_enc = labelenc.transform([skill for skill in row if skill in labelenc.classes_])
+    # Reject rows where none of the skills were encoded, may want to make this tune-able at some point
+    if len(row_enc) < 1:
+        titles.pop(index)  # Ensure len(titles) and len(data_encoded) are the same
+        continue
+    data_encoded.append(row_enc)
+print()
+
+# This is more pythonic but can't write progress!
+# data_encoded = [labelenc.transform([skill for skill in row if skill in labelenc.classes_]) for row in data]
+
+print("Preprocessing - creating dataframe")
 data_encoded_df = pd.DataFrame(0, index=np.arange(len(data_encoded)), columns=labelenc.classes_, dtype=int)
+
+print("Preprocessing - preparing dataframe")
 for index, entry in enumerate(data_encoded):
+    sys.stdout.write("\r")
+    sys.stdout.write("{:2.0f}".format(float(index/len(data_encoded))*100) + "%")
     data_encoded_df.iloc[index, entry] = 1
+print()
 data_encoded_df.insert(0, "job title", titles)
 
-# Remove skills that appear with low frequency
-data_flat_series = pd.Series(data_flat, dtype=str)
-mask = data_flat_series.isin(data_flat_series.value_counts()[data_flat_series.value_counts() > 10].index)
-cols_to_include = data_flat_series[mask].drop_duplicates()
-data_reduced_df = data_encoded_df[["job title"] + cols_to_include.tolist()]
-
 if writeDFs:
-    data_encoded_df.to_csv(r'./data_df_encoded.txt', sep=" ", index=False)
-    data_reduced_df.to_csv(r'./data_df_reduced.txt', sep=" ", index=False)
+    print("Preprocessing - writing df to file")
+    data_encoded_df.to_csv(r"D:/FutureFit/data_encoded_df.txt", sep=" ", index=False)
 
 print("Clustering")
 # Cluster!
-clustering = linkage(data_reduced_df[cols_to_include.tolist()].to_numpy(), method="single", metric="hamming")
-with open("matrix.txt", "w+") as file:
+clustering = linkage(data_encoded_df[labelenc.classes_].to_numpy(), method="single", metric="hamming")
+with open("D:/FutureFit/matrix.txt", "w+") as file:
     np.savetxt(file, clustering, fmt="%.5f")
-
