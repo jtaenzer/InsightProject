4465fb1108d8279fa772da21206d23b6e5311f3b
jtaenzer <mars.taenzer@gmail.com>
2020-05-31 19:00:59 -0400

    Merged the loops for encoding the data and building the dataframe, titles and encoded classes are now saved to pickles

--- JobTitleClustering/JobTitleClustering/linkage_example.py
+++ JobTitleClustering/JobTitleClustering/linkage_example.py
@@ -1,4 +1,5 @@
 import sys
+import pickle
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
@@ -10,13 +11,13 @@ writeDFs = True
 
 data_extractor = Pipeline("FutureFitAI_database", "talent_profiles")
 print("Getting skills")
-titles, data, data_flat = data_extractor.get_skills_by_titles(["data scientist", "data engineer"])
+# titles, data, data_flat = data_extractor.get_skills_by_titles(["data scientist", "data engineer"])
 # titles, data, data_flat = data_extractor.get_skills_by_titles(["data scientist", "data engineer", "marketing manager", "brand manager"])
-# titles, data, data_flat = data_extractor.get_all_skills()
+titles, data, data_flat = data_extractor.get_all_skills()
 
 print("Preprocessing - creating mask")
 data_flat_ser = pd.Series(data_flat, dtype=str)
-mask = data_flat_ser.isin(data_flat_ser.value_counts()[data_flat_ser.value_counts() > 10].index)
+mask = data_flat_ser.isin(data_flat_ser.value_counts()[data_flat_ser.value_counts() > 100].index)
 
 print("Preprocessing - reducing flat data")
 data_flat_reduced = data_flat_ser[mask]
@@ -24,43 +25,42 @@ data_flat_reduced = data_flat_ser[mask]
 print("Preprocessing - fitting integer encoding")
 labelenc = LabelEncoder()
 labelenc.fit(data_flat_reduced.drop_duplicates().tolist())
+with open("D:/FutureFit/encoding.pickle", "wb+") as file:
+    pickle.dump(labelenc.classes_, file)
 
-print("Preprocessing - encoding data row by row")
+print("Preprocessing - encoding data")
 # This is more pythonic but can't write progress!
 # data_encoded = [labelenc.transform([skill for skill in row if skill in labelenc.classes_]) for row in data]
-data_encoded = []
-for index, row in enumerate(data):
-    sys.stdout.write("\r")
-    sys.stdout.write("{:2.0f}".format(float(index/len(data))*100) + "%")
-    row_enc = labelenc.transform([skill for skill in row if skill in labelenc.classes_])
-    # Reject rows where none of the skills were encoded, may want to make this tune-able at some point
-    if len(row_enc) < 1:
-        titles.pop(index)  # Ensure len(titles) and len(data_encoded) are the same
-        continue
-    data_encoded.append(row_enc)
+data_encoded = pd.DataFrame(columns=labelenc.classes_, dtype=np.uint8)
+with open("D:/FutureFit/data_encoded.txt", "w+") as file:
+    for index, row in enumerate(data):
+        # Progress indicator
+        sys.stdout.write("\r")
+        sys.stdout.write("{:2.0f}".format(float(index/len(data))*100) + "%")
+
+        row_enc = labelenc.transform([skill for skill in row if skill in labelenc.classes_])
+        # Reject rows where none of the skills were encoded, may want to make this tune-able at some point
+        if len(row_enc) < 1:
+            titles.pop(index)  # Ensure len(titles) and len(data_encoded) are the same
+            continue
+        # Convert encoded row to an array of 0s and 1s corresponding to skills that were in the row
+        row_mhot = np.zeros(shape=len(labelenc.classes_), dtype=np.uint8)
+        row_mhot[row_enc] = 1
+        file.write("{}\n".format(", ".join(str(i) for i in row_mhot)))  # Write the row to the file
+        df_mhot = pd.DataFrame(data=row_mhot.reshape(1, -1), columns=labelenc.classes_, dtype=np.uint8)
+        data_encoded = data_encoded.append(df_mhot, ignore_index=True)  # Append the row to our dataframe
 print()
 
-print("Preprocessing - creating empty dataframe")
-data_encoded_df = pd.DataFrame(0, index=np.arange(len(data_encoded)), columns=labelenc.classes_, dtype=np.int8)
-
-print("Preprocessing - preparing dataframe")
-for index, entry in enumerate(data_encoded):
-    sys.stdout.write("\r")
-    sys.stdout.write("{:2.0f}".format(float(index/len(data_encoded))*100) + "%")
-    data_encoded_df.iloc[index, entry] = 1
-print()
-data_encoded_df.insert(0, "job title", titles)
-
-if writeDFs:
-    print("Preprocessing - writing df to file")
-    data_encoded_df.to_csv(r"D:/FutureFit/data_encoded_df.txt", sep=" ", index=False)
+with open("D:/FutureFit/titles.pickle", "wb+") as file:
+    pickle.dump(titles, file)
 
 print("Clustering")
 # Cluster!
-clustering = linkage(data_encoded_df[labelenc.classes_].to_numpy(), method="single", metric="hamming")
+clustering = linkage(data_encoded[labelenc.classes_].to_numpy(), method="single", metric="hamming")
 with open("D:/FutureFit/matrix.txt", "w+") as file:
     np.savetxt(file, clustering, fmt="%.5f")
 
+"""
 plt.figure(figsize=(25, 10))
 plt.title('Hierarchical Clustering Dendrogram')
 plt.xlabel('sample index')
@@ -69,7 +69,7 @@ dendrogram(
     clustering,
     leaf_rotation=90.,  # rotates the x axis labels
     leaf_font_size=8.,  # font size for the x axis labels
-    labels=titles,
 )
 plt.savefig("D:/FutureFit/dendrogram.png")
 plt.close()
+"""
