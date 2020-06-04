47f53835d02a8c7b315909d62d817c898a237a54
jtaenzer <mars.taenzer@gmail.com>
2020-06-04 15:37:42 -0400

    Improved preprocessing

--- JobTitleClustering/JobTitleClustering/classify_tfidf.py
+++ JobTitleClustering/JobTitleClustering/classify_tfidf.py
@@ -9,13 +9,15 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
 from pipeline import Pipeline
 
-remake_data = False
+get_raw_data = False
+clean_data = False
 remake_transform = True
 min_skill_freq = 10000
 min_title_freq = 1000
-min_skill_length = 10
+min_skill_length = 5
 titles_to_remove = ["owner", "president", "ceo", "manager", "founder", "supervisor", "business owner", "intern", "co-founder", "président", "propriétaire"]
 save_path = "D:/FutureFit/classifying_tfidf_canada/skill_freq_{0}_skill_length_{1}_title_freq_{2}/"\
     .format(min_skill_freq, min_skill_length, min_title_freq)
@@ -24,50 +26,72 @@ save_path = "D:/FutureFit/classifying_tfidf_canada/skill_freq_{0}_skill_length_{
 if not os.path.exists(save_path):
     os.makedirs(save_path)
 
-if remake_data:
+if get_raw_data:
     data_extractor = Pipeline("FutureFitAI_database", "talent_profiles")
     print("Retrieving data from DB")
-    titles, data, data_flat = data_extractor.get_all_skills(min_skill_length=min_skill_length)
+    titles, data = data_extractor.get_all_skills(min_skill_length=min_skill_length)
     if len(titles) != len(data):
         print("Length of titles inconsistent with length of data, this should never happen.")
         sys.exit(2)
-    # Create vocab for TF-IDF
-    vocab_for_counts = pd.Series(data_flat, dtype=str)
     # Dump data to binaries so it can be re-used later
     print("Dumping data to binaries for later use")
     dump(data, save_path + "data_join.joblib")
-    dump(vocab_for_counts.drop_duplicates().tolist(), save_path + "vocab_for_counts.joblib")
     dump(titles, save_path + "titles_for_plotting.joblib")
 else:
     print("Loading data from binaries")
     data = load(save_path + "data_join.joblib")
-    vocab_for_counts = pd.Series(load(save_path + "vocab_for_counts.joblib"), dtype=str)
     titles = load(save_path + "titles_for_plotting.joblib")
 
-# Keep only most frequent titles
-titles_ser = pd.Series(titles, dtype=str)
-mask = titles_ser.isin(titles_ser.value_counts()[titles_ser.value_counts() > min_title_freq].index)
-titles_reduced = titles_ser[mask]
+if clean_data:
+    # Keep only most frequent titles
+    titles_ser = pd.Series(titles, dtype=str)
+    mask = titles_ser.isin(titles_ser.value_counts()[titles_ser.value_counts() > min_title_freq].index)
+    titles_reduced = titles_ser[mask]
 
-data_clean_min_title_freq = list()
-print("Cleaning data -- title min frequency")
-for index, check in enumerate(mask):
-    if check:
-        data_clean_min_title_freq.append(data[index])
+    data_clean_min_title_freq = list()
+    print("Cleaning data -- title min frequency")
+    for index, check in enumerate(mask):
+        if check:
+            data_clean_min_title_freq.append(data[index])
 
-data_clean = list()
-print("Cleaning data -- removing noisy titles")
-for index, title in enumerate(titles_reduced):
-    if title not in titles_to_remove:
-        data_clean.append(data_clean_min_title_freq[index])
+    data_clean = list()
+    print("Cleaning data -- removing noisy titles")
+    for index, title in enumerate(titles_reduced):
+        if title not in titles_to_remove:
+            data_clean.append(data_clean_min_title_freq[index])
 
-for drop_title in titles_to_remove:
-    titles_reduced = titles_reduced[titles_reduced != drop_title]
+    for drop_title in titles_to_remove:
+        titles_reduced = titles_reduced[titles_reduced != drop_title]
+
+    print("Creating vocab for tokenization and converting data lists to strings")
+    data_flat = list()
+    data_clean_str = list()
+    for index, row in enumerate(data_clean):
+        if index % 1000 == 0:
+            sys.stdout.write("\r")
+            sys.stdout.write("{:2.0f}".format(float(index / len(data_clean)) * 100) + "%")
+        data_flat.extend(row)
+        data_clean_str.append(" ".join(row))
+    print()
+    vocab_for_counts = pd.Series(data_flat, dtype=str)
+    print("Dumping clean data to binaries for later use")
+    dump(data_clean_str, save_path + "data_clean_str.joblib")
+    dump(titles_reduced, save_path + "titles_reduced.joblib")
+    dump(vocab_for_counts, save_path + "vocab_for_counts.joblib")
+else:
+    print("Loading cleaned data from binaries")
+    titles_reduced = load(save_path + "titles_reduced.joblib")
+    data_clean_str = load(save_path + "data_clean_str.joblib")
+    vocab_for_counts = load(save_path + "vocab_for_counts.joblib")
+
+if len(titles_reduced) != len(data_clean_str):
+    print("Length of titles inconsistent with length of clean data, this should never happen.")
+    sys.exit(2)
 
 print("Transforming data to count matrix")
 mask = vocab_for_counts.isin(vocab_for_counts.value_counts()[:min_skill_freq].index)
 count_vectorizer = CountVectorizer(vocabulary=vocab_for_counts[mask].drop_duplicates().tolist())
-data_count_matrix = count_vectorizer.transform(data_clean)
+data_count_matrix = count_vectorizer.transform(data_clean_str)
 dump(count_vectorizer, save_path + "count_vectorizer.joblib")
 
 print("Transforming data to tfidf matrix")
@@ -125,3 +149,5 @@ mlp = MLPClassifier(hidden_layer_sizes=(X_train.shape[1], int((2/3)*X_train.shap
 print("Fitting model")
 mlp.fit(X_train, y_train)
 dump(mlp, save_path + "model.joblib")
+predictions = mlp.predict(X_test)
+print("accuracy score:", accuracy_score(y_test,predictions))
