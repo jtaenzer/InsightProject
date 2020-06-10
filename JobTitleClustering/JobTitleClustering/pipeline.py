3093d801276e2db089294eb7a04dde9b5c77dde5
jtaenzer <mars.taenzer@gmail.com>
2020-06-10 17:55:58 -0400

    Added lots of comments, reorganized drop_titles_from_data based on min_title_freq

--- JobTitleClustering/JobTitleClustering/pipeline.py
+++ JobTitleClustering/JobTitleClustering/pipeline.py
@@ -9,16 +9,22 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 
 """
-pipeline.py extracts information from the mongoDB
+pipeline.py 
+-extracts data from the mongoDB
+-cleans the extracted data based on hyperparameter choices
+-transforms the data with sklearn's CountVectorizer and TfidfTransformer
+
+The full pipeline for clustering can be run via run_clustering_pipeline
 """
 
 
 class Pipeline:
-    def __init__(self, db_name, collection_name, client_url="mongodb://localhost:27017/", binary_path="."):
+    def __init__(self, db_name, collection_name, client_url="mongodb://localhost:27017/", binary_path=""):
         self.client = MongoClient(client_url)
         # Consider adding some protections against bad input
         self.db = self.client[db_name]
         self.collection = self.db[collection_name]
+        # If this is left as the default empty string, binaries won't be dumped in run_clustering_pipline
         self.binary_path = binary_path
 
         # Private vars to hold the data
@@ -125,7 +131,7 @@ class Pipeline:
             print("Pipeline.get_skills_by_titles: len(titles) != len(skills_data), this should never happen.")
             sys.exit(2)
 
-    # Similiar to get_all_skills above but the job title is retrieved from the primary field
+    # Similar to get_all_skills above but the job title is retrieved from the primary field
     def get_all_skills_primary(self, min_skill_length=5):
         conditions = {"$and": [{"skills": {"$exists": True, "$ne": []}},
                                {"primary.job.title": {"$exists": True, "$ne": None}},
@@ -197,9 +203,38 @@ class Pipeline:
         print()
         return titles
 
+    # Clean the data / titles based on
+    # (1) An input list of titles to drop
+    # (2) Removal of "tails" based on some minimum frequency
+    # This function suffers from the confusion about the titles typing (is it a list or a series?) -- to improve
+    def drop_titles_from_data(self, titles_to_drop, min_title_freq=0):
+        # Start by putting the raw data/titles into the clean private vars in case we don't do any cleaning here
+        # This is pretty bad since it assumes this will always be the first cleaning step -- rethink
+        self.titles_clean = pd.Series(self.titles_raw, dtype=str)
+        self.data_clean = self.data_raw
+        # Remove "tails" in the titles from the data
+        if min_title_freq > 0:
+            mask = self.titles_clean.isin(self.titles_clean.value_counts()[self.titles_clean.value_counts() > min_title_freq].index)
+            self.titles_clean = self.titles_clean[mask]
+            self.data_clean = self.clean_data_list_by_mask(mask, self.data_clean)
+            self.cleaning_level["title_freq"] = True
+        # If titles to drop wasn't provided stop here
+        if not titles_to_drop:
+            return
+        # Remove titles in the titles_to_drop list from the titles/data
+        mask = ~self.titles_clean.isin(titles_to_drop)
+        print(len(mask))
+        self.titles_clean = self.titles_clean[mask]
+        self.data_clean = self.clean_data_list_by_mask(mask, self.data_clean)
+        self.cleaning_level["title_ignore_list"] = True
+
+    # In order to use CountVectorizer we need a vocabulary (not necessarily true if all skills are kept...)
+    # And we need to convert our data from a list of lists to a list of strings/"documents"
+    # Any skills in thee data that are not in the vocab we build here will be dropped by CountVectorizer
     def prepare_data_for_count_vectorizer(self, skill_depth=10000):
         skills_flat = list()
         data_join = list()
+        # If we haven't done any cleaning previously, start from the raw data
         if not self.data_clean:
             self.titles_clean = self.titles_raw
             self.data_clean = self.data_raw
@@ -212,34 +247,25 @@ class Pipeline:
         mask = skills_flat.isin(skills_flat.value_counts()[:skill_depth].index)
         self.skills_vocabulary = skills_flat[mask].drop_duplicates().tolist()
 
+    # Initialize the CountVectorizer with our vocab and transform the data
+    # self.count_vectorizer can be dumped to a binary for later use by calling dump_binaries()
     def setup_count_vectorizer_and_transform(self):
         self.count_vectorizer = CountVectorizer(vocabulary=self.skills_vocabulary, token_pattern=self.token_pattern)
         self.data_count_matrix = self.count_vectorizer.transform(self.data_clean)
 
+    # Initialize the TfidfTransformer and fit_transform the count matrix created by setup_count_vectorizer_and_transform
+    # Calling these methods out of order will not work! Add protections
     def setup_tfidf_transformer_and_fit_transform(self, data):
         self.tfidf_transformer = TfidfTransformer()
         self.tfidf_transformer.fit(data)
         self.data_tfidf_matrix = self.tfidf_transformer.transform(data)
 
-    def drop_titles_from_data(self, titles_to_drop, min_title_freq=0):
-        if not titles_to_drop:
-            self.titles_clean = pd.Series(self.titles_raw, dtype=str)
-            self.data_clean = self.data_raw
-            self.cleaning_level["title_ignore_list"] = True
-            return
-        titles_series = pd.Series(self.titles_raw, dtype=str)
-        mask = ~titles_series.isin(titles_to_drop)
-        self.titles_clean = titles_series[mask]
-        self.data_clean = self.clean_data_list_by_mask(mask, self.data_raw)
-        self.cleaning_level["title_ignore_list"] = True
-        if min_title_freq > 0:
-            mask = self.titles_clean.isin(self.titles_clean.value_counts()[self.titles_clean.value_counts() > min_title_freq].index)
-            self.titles_clean = self.titles_clean[mask]
-            self.data_clean = self.clean_data_list_by_mask(mask, self.data_clean)
-            self.cleaning_level["title_freq"] = True
-
+    # Drop rows from our matrices and data/titles lists that don't have a row sum > min_skill_length in the count matrix
+    # Likely the data was already filtered by some min_skill_length during the DB extraction
+    # However, now that we've established our CountVectorizer vocabulary, we can re-apply this requirement
     def drop_matrix_rows_by_sum(self, min_skill_length=5):
         mask = (self.data_count_matrix.sum(axis=1) >= min_skill_length).reshape(1, -1).tolist()[0]
+        # This try/except is only necessary because self.titles_clean isn't consistently typed... should fix
         try:
             self.titles_clean = self.titles_clean[mask]
         except TypeError:
@@ -249,6 +275,8 @@ class Pipeline:
         self.data_tfidf_matrix = self.data_tfidf_matrix.toarray()[mask]  # This may try to allocate alot of memory...
         self.cleaning_level["count_vec_sum"] = True
 
+    # Dump class vars to binaries for later use
+    # Mostly exists for cases where a partial pre-pocessing needs to be re-started from some interim step
     def dump_binaries(self):
         if not os.path.exists(self.binary_path):
             os.makedirs(self.binary_path)
@@ -256,6 +284,7 @@ class Pipeline:
             dump(self.titles_clean, self.binary_path + "titles_clean.joblib")
         if self.data_clean:
             dump(self.data_clean, self.binary_path + "data_clean.joblib")
+            dump(self.cleaning_level, self.binary_path + "cleaning_level_dict.joblib")
         if self.skills_vocabulary:
             dump(self.skills_vocabulary, self.binary_path + "skills_vocabulary.joblib")
         if self.tfidf_transformer:
@@ -265,21 +294,24 @@ class Pipeline:
         if self.data_count_matrix.shape[0] > 0:
             dump(self.data_count_matrix, self.binary_path + "data_count_matrix.joblib")
 
+    # Load existing binaries into class vars
     def load_binaries(self):
         self.titles_clean = load(self.binary_path + "titles_clean.joblib")
         self.data_clean = load(self.binary_path + "data_clean.joblib")
+        self.cleaning_level = load(self.binary_path + "cleaning_level_dict.joblib")
         self.skills_vocabulary = load(self.binary_path + "skills_vocabulary.joblib")
         self.tfidf_transformer = load(self.binary_path + "tfidf_transformer.joblib")
         self.data_tfidf_matrix = load(self.binary_path + "data_tfidf_matrix.joblib")
         self.data_count_matrix = load(self.binary_path + "data_count_matrix.joblib")
 
-    def run_clustering_pipeline(self, min_skill_length=5, drop_titles=list(), skill_depth=10000, verbose=0):
+    # Run the full clustering pipeline in order
+    def run_clustering_pipeline(self, min_skill_length=5, drop_titles=list(), skill_depth=10000, min_title_freq=3, verbose=0):
         if verbose:
             print("Getting raw data from the DB")
         self.get_all_skills_primary(min_skill_length=min_skill_length)
         if verbose:
             print("Dropping titles from bad title list from data")
-        self.drop_titles_from_data(drop_titles)
+        self.drop_titles_from_data(drop_titles, min_title_freq=min_title_freq)
         if verbose:
             print("Preparing data for CountVectorizer and TfidfTransformer")
         self.prepare_data_for_count_vectorizer(skill_depth=skill_depth)
@@ -294,10 +326,13 @@ class Pipeline:
         self.drop_matrix_rows_by_sum(min_skill_length=min_skill_length)
         if verbose:
             print("Dumping binaries")
-        self.dump_binaries()
+        if self.binary_path:
+            self.dump_binaries()
         if verbose:
             print("Pipeline complete!")
 
+    # This function exists because the data list is generally too large to be put in a pandas series/dataframe
+    # Provides the same functionality as df = df[mask] but in a for loop...
     @staticmethod
     def clean_data_list_by_mask(mask, data):
         if len(mask) != len(data):
