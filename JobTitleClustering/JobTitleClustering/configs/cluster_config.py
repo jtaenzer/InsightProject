7aa71adf956f0c696113227a71aa05a901ee6ca7
jtaenzer <mars.taenzer@gmail.com>
2020-06-12 09:35:13 -0400

    Added binary path to config

--- JobTitleClustering/JobTitleClustering/configs/cluster_config.py
+++ JobTitleClustering/JobTitleClustering/configs/cluster_config.py
@@ -2,16 +2,23 @@
 database_name = "FutureFitAI_database"
 collection_name = "talent_profiles"
 
+# Where to dump binaries, leave as empty string if you don't want to save them
+binary_path = ""
+
 # How deep into the skill list do we want to go (sorted by value counts)
 # This will establish the width of matrix input to the clustering
 min_skill_depth = 5000
 # How deep in the title list we want to to go (sorted by value counts)
 # Dropping infrequent titles as a form of cleaning
-min_title_freq = 350
+min_title_freq = 5
 # Minimum number of skills a profile has to contain to included in the data
 min_skill_length = 10
 # Number of clusters to stop the clustering at
 n_cluster_stop = 1
+# Distance measure to use, other options are "cosine", "l1", "cityblock" -- euclidean works best in small scale tests
+affinity = "euclidean"
+# Linkage strategy to use, other options are "single", "complete", "average" -- ward works best in small scale tests
+linkage = "ward"
 # List of "bad" job titles to drop from the data -- built by hand and should be subject to re-investigation
 titles_to_drop = ["ceo",
                   "chief executive officer",
@@ -127,5 +134,9 @@ titles_to_drop = ["ceo",
                   "consultant",
                   "senior consultant",
                   #"teacher",  # pulled out because of high counts -- should be clustered later
-                  #"realtor",  # pulled out because of high counts -- should be clustered later
+                  "realtor",  # pulled out because of high counts -- should be clustered later
+                  "customer service representative", # pulled out because of high counts -- should be clustered later
+                  "customer service rep", # pulled out because of high counts -- should be clustered later
+                  "sales representative", # pulled out because of high counts -- should be clustered later
+                  "sales rep", # pulled out because of high counts -- should be clustered later
                   ]
