be2bcd1817259a9cb7e5f82ec737615ef42de0c1
jtaenzer <mars.taenzer@gmail.com>
2020-06-23 09:34:06 -0400

    Added some new configurables

--- src/configs/analysis_config.py
+++ src/configs/analysis_config.py
@@ -1,13 +1,23 @@
 # Path to binaries to load
-binary_path = "./binaries/"
+binary_path = "D:/FutureFit/clustering_tfidf_canada/subsampled_120kprofiles/binaries/"
 # Path to save plots
-plot_path = "./plots/"
+plot_path = "D:/FutureFit/clustering_tfidf_canada/subsampled_120kprofiles/plots/"
 
-# How many clusters do we want to find?
+# More information will be printed out
+verbose = True
+# Create histograms and word clouds
+plotting = True
+
+# This should be the same as what was used for the clustering
+min_skill_length = 10
+# How many clusters do we want to find? **NOTE** this is a soft target and may never be reached
 n_target_clusters = 200
 # Do we want to ignore small clusters?
 min_clus_size = 100
-# Minimum purity -- to find good quality clusters we want them to be dominated by a particular title
+# Minimum purity -- to find "good" clusters we want them to be dominated by a particular title
+# Purity = 100 * (most_frequent_title_count / cluster_size)
 min_purity = 10
 # How deep into skills_series.value_counts() should we go when creating our core skills
 core_skills_depth = 15
+# Training/testing fraction for classification
+train_test_frac = 0.5
