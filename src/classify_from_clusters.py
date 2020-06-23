bab36ce8d81fffcc21f9f41e9fdd6baf1ade9de3
jtaenzer <mars.taenzer@gmail.com>
2020-06-23 09:32:10 -0400

    Using AnalysisTools

--- src/classify_from_clusters.py
+++ src/classify_from_clusters.py
@@ -1,114 +1,52 @@
 import pandas as pd
 import numpy as np
 from joblib import dump, load
-
 from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
+from pipeline import Pipeline
+from analysis_tools import AnalysisTools
+import configs.analysis_config as cfg
 
-binary_path = "./binaries/"
-# How many clusters do we want to find?
-n_target_clusters = 300
-# Do we want to ignore small clusters?
-min_clus_size = 200
-# Minimum purity -- to find good quality clusters we want them to be dominated by a particular title
-min_purity = 10
-# Depth in skills list to be considered core skills
-core_skills_depth = 10
-# Train/test frac
-train_test_frac = 0.8
+# Create AnalysisTools object for later use
+tools = AnalysisTools()
 
-# Load binaries
-clustering = load(binary_path + "clustering_model.joblib")
-titles = load(binary_path + "titles_subsampled.joblib")
-titles = titles.tolist()  # Titles was saved as a pandas series
-labelenc = load(binary_path + "label_encoder.joblib")
-title_encoding = labelenc.classes_.tolist()  # classes_ is a numpy array but we want to make use of list.index()
-data_dict = load(binary_path + "data_subsampled.joblib")
-count_vectorizer = load(binary_path + "count_vectorizer.joblib")
-tfidf_transformer = load(binary_path + "tfidf_transformer.joblib")
+# Use the pipeline for loading binaries and to store some objects
+data_pipeline = Pipeline("", "", binary_path=cfg.binary_path)
+data_pipeline.load_binaries()
+clustering_model = load(cfg.binary_path + "clustering_model.joblib")
+titles_subsampled = data_pipeline.titles_subsampled.tolist()
+title_encoding = data_pipeline.label_encoder.classes_.tolist()
 
 # Rebuild the tfidf transformed matrix
 print("Rebuilding clustering input")
-mat_list = list()
-for key in data_dict.keys():
-    cluster_matrix = count_vectorizer.transform(data_dict[key]).toarray()
-    cluster_matrix = cluster_matrix[np.sum(cluster_matrix, axis=1) > 10]
-    cluster_matrix = tfidf_transformer.transform(cluster_matrix).toarray()
-    mat_list.append(cluster_matrix)
-data_tfidf_matrix = np.concatenate([mat for mat in mat_list], axis=0)
+matrix_list = list()
+for key in data_pipeline.data_subsampled.keys():
+    cluster_matrix = data_pipeline.count_vectorizer.transform(data_pipeline.data_subsampled[key]).toarray()
+    cluster_matrix = cluster_matrix[np.sum(cluster_matrix, axis=1) > cfg.min_skill_length]
+    cluster_matrix = data_pipeline.tfidf_transformer.transform(cluster_matrix).toarray()
+    matrix_list.append(cluster_matrix)
+data_tfidf_matrix = np.concatenate([matrix for matrix in matrix_list], axis=0)
 
+# Find the core skills for each title that we encoded before clustering
 print("Getting core skills")
+core_skills_dict = tools.build_core_skills(data_pipeline.data_subsampled, title_encoding, depth=cfg.core_skills_depth)
+# Re-build the tree-like structure of the clustering so we can unwind it
+print("Building clustering tree")
+clustering_tree = tools.build_clustering_tree(clustering_model, titles_subsampled, title_encoding)
+# Unwind the clustering tree looking for clusters with some minimum size and purity
+print("Finding pure clusters")
+pure_clusters = tools.find_pure_clusters(clustering_model, clustering_tree,
+                                         cfg.n_target_clusters, cfg.min_clus_size, cfg.min_purity)
+
 # Create and dump core_skills_dict
 core_skills_dict = dict()
-for key in data_dict.keys():
+for key in data_pipeline.data_subsampled.keys():
     tmp_skills = list()
-    for index, row in enumerate(data_dict[key]):
+    for index, row in enumerate(data_pipeline.data_subsampled[key]):
         tmp_skills.extend(row.split(", "))
     tmp_series = pd.Series(tmp_skills, dtype=str)
-    core_skills_dict[key] = tmp_series.value_counts()[:core_skills_depth].index.tolist()
-dump(core_skills_dict, binary_path + "core_skills_dict.joblib")
-
-print("Building clustering tree")
-# Generate a dictionary holding the tree-like structure of the clustering tree
-# Could also be useful to have a field that holds the direct direct descendants of each cluster
-children = clustering.children_
-clustering_tree = dict()
-for index, row in enumerate(children):
-    # Check if we've found a singleton, in that case the index is exactly whats in the clustering matrix
-    # Title and skills can be taken directly from the data
-    if row[0] < clustering.n_leaves_:
-        titles1 = [title_encoding[int(titles[int(row[0])])]]
-        indices1 = [int(row[0])]
-    # If we haven't found a singleton, fill indices/titles/skills from a previous iteration of this loop!
-    # Note: since we're looping through the clustering matrix in order, singletons will always be added first
-    else:
-        titles1 = clustering_tree[int(row[0])]["child_titles"]
-        indices1 = clustering_tree[int(row[0])]["child_indices"]
-    # Same as above but for the other index in the clustering matrix
-    if row[1] < clustering.n_leaves_:
-        titles2 = [title_encoding[int(titles[int(row[1])])]]
-        indices2 = [int(row[1])]
-    else:
-        titles2 = clustering_tree[int(row[1])]["child_titles"]
-        indices2 = clustering_tree[int(row[1])]["child_indices"]
-
-    clustering_tree[1 + index + len(children)] = {"child_titles": titles1+titles2,
-                                                  "child_indices": indices1+indices2}
-
-print("Finding pure clusters")
-# Define the starting point to unwind our clustering tree -- this could be tunable
-# For now we are starting from the top-most cluster which should contain all possible children
-clusters = list()
-clusters.append(children[children.shape[0] - 1][0])
-clusters.append(children[children.shape[0] - 1][1])
-# Unwind the clustering tree we build earlier until we arrive at n_target_clusters or run out of clusters
-# Deciding whether to keep a cluster for analysis is based on its purity:
-# purity = 100*titles_ser.value_counts()[0]/titles_ser.value_counts().sum()
-# I.E. What fraction of the cluster is the title with the highest count?
-# Some caveats:
-# (1) Because we generally don't want to see singletons or "small" clusters, they are dropped
-#     Depending on n_target_clusters and nature of the clustering, this can lead to never reaching n_target_clusters
-#     Hence the if len(clusters) == 0: break
-# (2) Depending on min_purity this can remove large chunks of the data and up with only very small clusters
-#     There is no way obvious way around this, tuning n_target_clusters and min_purity is necessary to get good output
-pure_clusters = list()
-while len(pure_clusters) < n_target_clusters:
-    if len(clusters) == 0:
-        break
-    tmp_clusters = list()
-    for cluster in clusters:
-        titles_ser = pd.Series(clustering_tree[cluster]["child_titles"], dtype=str)
-        purity = 100*titles_ser.value_counts()[0]/titles_ser.value_counts().sum()
-        if purity > min_purity:
-            pure_clusters.append(cluster)
-        else:
-            left = children[cluster - children.shape[0] - 1][0]
-            if left > children.shape[0] and len(clustering_tree[left]["child_indices"]) > min_clus_size:
-                tmp_clusters.append(left)
-            right = children[cluster - children.shape[0] - 1][1]
-            if right > children.shape[0] and len(clustering_tree[right]["child_indices"]) > min_clus_size:
-                tmp_clusters.append(right)
-    clusters = tmp_clusters
+    core_skills_dict[key] = tmp_series.value_counts()[:cfg.core_skills_depth].index.tolist()
+dump(core_skills_dict, cfg.binary_path + "core_skills_dict.joblib")
 
 print("Preparing training data")
 # Create training data from clusters
@@ -122,7 +60,7 @@ for cluster in pure_clusters:
     output_column = np.array([[cluster_label_enc]*cluster_matrix.shape[0]]).reshape(-1, 1)
     cluster_matrix = np.concatenate((cluster_matrix, output_column), axis=1)
     np.random.shuffle(cluster_matrix)
-    training_data_list.append(cluster_matrix[:int(train_test_frac*cluster_matrix.shape[0]), :])
+    training_data_list.append(cluster_matrix[:int(cfg.train_test_frac*cluster_matrix.shape[0]), :])
 
 training_data_matrix = np.concatenate([matrix for matrix in training_data_list], axis=0)
 np.random.shuffle(training_data_matrix)
@@ -134,7 +72,7 @@ X_train = scaler.fit_transform(X_train)
 print("Fitting model")
 print(X_train.shape)
 mlp = MLPClassifier(hidden_layer_sizes=(X_train.shape[1], int((2/3)*X_train.shape[1]),
-                                        len(labelenc.classes_)), max_iter=1000, verbose=True)
+                                        len(data_pipeline.label_encoder.classes_)), max_iter=1000, verbose=True)
 
 mlp.fit(X_train, y_train)
-dump(mlp, binary_path + "MLPClassifier_model.joblib")
+dump(mlp, cfg.binary_path + "MLPClassifier_model.joblib")
